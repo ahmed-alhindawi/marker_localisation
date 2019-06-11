7a201741ab2d09ef3e56c664303fd17a01be10f8
Ahmed Al-Hindawi <a.al-hindawi@imperial.ac.uk>
2019-06-11 18:08:56 +0100

    added launch files

---
+++ src/ArUcoMarkerPoseNode.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+
+"""
+@Ahmed Al-Hindawi <a.al-hindawi@imperial.ac.uk>
+"""
+
+from __future__ import print_function
+
+import rospy
+from tf import TransformListener, TransformBroadcaster
+from tf import transformations
+import cv2.aruco as aruco
+import cv2
+from cv_bridge import CvBridge
+import image_geometry
+import numpy as np
+
+from sensor_msgs.msg import Image, CameraInfo
+from geometry_msgs.msg import Point, PoseStamped, Quaternion
+from marker_localisation.msg import MarkerTagDetection, MarkerTagDetectionArray
+
+
+class ArUcoBoardNode(object):
+    def __init__(self):
+        self._tf_broadcaster = TransformBroadcaster()
+        self._tf_listener = TransformListener()
+        self._cv_bridge = CvBridge()
+        self._publish_tf = rospy.get_param("~publish_tf", default=True)
+        _required_dict = rospy.get_param("~dictionary", default="original").lower()
+        _allowed_dicts = ["ORIGINAL", "4x4", "5x5", "6x6"]
+
+        if _required_dict == "original":
+            self._dict = aruco.Dictionary_get(aruco.DICT_ARUCO_ORIGINAL)
+        elif _required_dict == "4x4":
+            self._dict = aruco.Dictionary_get(aruco.DICT_4X4_50)
+        elif _required_dict == "5x5":
+            self._dict = aruco.Dictionary_get(aruco.DICT_5X5_50)
+        elif _required_dict == "6x6":
+            self._dict = aruco.Dictionary_get(aruco.DICT_6X6_50)
+        else:
+            raise ValueError("Required ARUCO dictionary not available. Available ones are: {}".format(_allowed_dicts))
+
+        self._marker_length = rospy.get_param("~marker_length", default=0.10)
+
+        rospy.loginfo("Waiting for camera info messages")
+        self.camera = image_geometry.PinholeCameraModel()
+        cam_info_msg = rospy.wait_for_message(
+            rospy.get_param("~camera_info_topic", "camera_info"), CameraInfo, timeout=None)
+        self.camera.fromCameraInfo(cam_info_msg)
+        rospy.loginfo("...Received")
+
+        rospy.Subscriber(rospy.get_param("~camera_image_topic", default="/image"), Image, self.publish_marker_transform)
+        self._marker_pub = rospy.Publisher("/tags", MarkerTagDetectionArray, queue_size=10)
+
+    def publish_marker_transform(self, cam_img):
+        frame = self._cv_bridge.imgmsg_to_cv2(cam_img)
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # identify markers and
+        corners, ids, rejected_img_points = aruco.detectMarkers(gray, self._dict)
+
+        if ids is not None and len(ids) > 0:
+            rvec, tvec, _ = aruco.estimatePoseSingleMarkers(corners, self._marker_length, self.camera.K, self.camera.D)
+
+            tags = []
+            for i, marker_id in enumerate(ids):
+                trans = tvec[i][0]
+                rot = rvec[i][0]
+                angle = np.linalg.norm(rot)
+                axis = rot.flatten() / angle
+
+                quaternion = transformations.quaternion_about_axis(angle, axis)
+
+                tag_corners = corners[i].reshape(4, 2)
+                c1 = Point()
+                c1.x = tag_corners[0][0]
+                c1.y = tag_corners[0][1]
+
+                c2 = Point()
+                c2.x = tag_corners[1][0]
+                c2.y = tag_corners[1][1]
+
+                c3 = Point()
+                c3.x = tag_corners[2][0]
+                c3.y = tag_corners[2][1]
+
+                c4 = Point()
+                c4.x = tag_corners[3][0]
+                c4.y = tag_corners[3][1]
+
+                tag = MarkerTagDetection()
+                tag.id = marker_id
+                tag.corners2d = [c1, c2, c3, c4]
+                tag.tag_size = self._marker_length
+                tag.pose = PoseStamped()
+                tag.pose.pose.position = Point(*trans)
+                tag.pose.pose.orientation = Quaternion(*quaternion)
+                tag.pose.header = cam_img.header
+
+                tags.append(tag)
+
+                if self._publish_tf:
+                    self._tf_broadcaster.sendTransform(translation=trans,
+                                                       rotation=quaternion,
+                                                       time=cam_img.header.stamp,
+                                                       child="ar_marker_{}".format(marker_id[0]),
+                                                       parent=cam_img.header.frame_id)
+
+                # # just while we're here, lets view it
+                # pts = np.array(corners).astype(np.int32).reshape((-1, 1, 2))
+                # cv2.polylines(frame, [pts], True, (0, 255, 255))
+                # cv2.imshow("marker", frame)
+                # cv2.waitKey(1)
+
+            mtda = MarkerTagDetectionArray()
+            mtda.detections = tags
+            self._marker_pub.publish(mtda)
+
+
+# Main function
+if __name__ == '__main__':
+    # Initialize the node
+    rospy.init_node('aruco_marker_pose_publisher')
+
+    # Create the camera publisher
+    tap = ArUcoBoardNode()
+    rospy.spin()
--- src/ChArUcoBoardPoseNode.py
+++ src/ChArUcoBoardPoseNode.py
@@ -3,19 +3,17 @@
 from __future__ import print_function
 
 import rospy
-from tf import TransformListener, TransformBroadcaster, LookupException, ExtrapolationException
-from tf2_py import TransformException
+from tf import TransformListener, TransformBroadcaster
 from tf import transformations
-import rospkg
 import cv2.aruco as aruco
 import cv2
-from cv_bridge import CvBridgeError, CvBridge
+from cv_bridge import CvBridge
 import image_geometry
-import os
-import yaml
 import numpy as np
 
 from sensor_msgs.msg import Image, CameraInfo
+from geometry_msgs.msg import Point, PoseStamped, Quaternion
+from marker_localisation.msg import MarkerTagDetection
 
 
 class ChArUcoBoardNode(object):
@@ -23,29 +21,50 @@ class ChArUcoBoardNode(object):
         self._tf_broadcaster = TransformBroadcaster()
         self._tf_listener = TransformListener()
         self._cv_bridge = CvBridge()
+        self._publish_tf = rospy.get_param("~publish_tf", default=True)
 
         rospy.loginfo("Waiting for camera info messages")
         self.camera = image_geometry.PinholeCameraModel()
         cam_info_msg = rospy.wait_for_message(
-            rospy.get_param("~camera_info_topic", "camera_info"), CameraInfo, timeout=None)
+            rospy.get_param("~camera_info_topic", "/camera_info"), CameraInfo, timeout=None)
         self.camera.fromCameraInfo(cam_info_msg)
         rospy.loginfo("...Received")
 
-        self._world_quat = np.array([0.0, 1.0, 0.0, 0.0])
+        _required_dict = rospy.get_param("~dictionary", default="original").lower()
+        _allowed_dicts = ["ORIGINAL", "4x4", "5x5", "6x6"]
 
-        board_config_path = os.path.join(rospkg.RosPack().get_path("visual_attention_setup"), "model", "charuco_board.yaml")
-        with open(board_config_path, 'r') as stream:
-            try:
-                board_config = yaml.load(stream)
-                self._name = board_config["name"]
-                self._dict = aruco.Dictionary_get(aruco.DICT_ARUCO_ORIGINAL)
-                self._board = aruco.CharucoBoard_create(board_config['columns'], board_config['rows'],
-                                                        board_config['square_size'], board_config['marker_size'],
-                                                        self._dict)
+        if _required_dict == "original":
+            self._dict = aruco.Dictionary_get(aruco.DICT_ARUCO_ORIGINAL)
+        elif _required_dict == "4x4":
+            self._dict = aruco.Dictionary_get(aruco.DICT_4X4_50)
+        elif _required_dict == "5x5":
+            self._dict = aruco.Dictionary_get(aruco.DICT_5X5_50)
+        elif _required_dict == "6x6":
+            self._dict = aruco.Dictionary_get(aruco.DICT_6X6_50)
+        else:
+            raise ValueError("Required CHARUCO dictionary not available. Available ones are: {}".format(_allowed_dicts))
 
-                rospy.Subscriber("image", Image, self.publish_marker_transform)
-            except yaml.YAMLError as exc:
-                print(exc)
+        default_config = {
+            'name': 'board1',
+            'columns': 3,
+            'rows': 3,
+            'square_size': 0.0551,
+            'marker_size': 0.0412
+        }
+
+        self._board_config = rospy.get_param("~board_configuration", default=default_config)
+
+        assert self._board_config is not None, "Board Configuration must be present"
+        assert self._dict is not None, "Dictionary must be present"
+
+        self._board = aruco.CharucoBoard_create(self._board_config['columns'],
+                                                self._board_config['rows'],
+                                                self._board_config['square_size'],
+                                                self._board_config['marker_size'],
+                                                self._dict)
+
+        rospy.Subscriber(rospy.get_param("~camera_image_topic", default="/image"), Image, self.publish_marker_transform)
+        self._marker_pub = rospy.Publisher("/tags", MarkerTagDetection, queue_size=10)
 
     def publish_marker_transform(self, cam_img):
         frame = self._cv_bridge.imgmsg_to_cv2(cam_img)
@@ -55,31 +74,61 @@ class ChArUcoBoardNode(object):
         # identify markers and
         corners, ids, rejected_img_points = aruco.detectMarkers(gray, self._dict)
 
-        if ids is not None:
+        if ids is not None and len(ids) > 0:
             ret, ch_corners, ch_ids = aruco.interpolateCornersCharuco(corners, ids, gray, self._board)
-
             # if there are enough corners to get a reasonable result
-            if ret > 7:
-                aruco.drawDetectedCornersCharuco(frame, ch_corners, ch_ids, (0, 0, 255))
-            retval, rvec, tvec = aruco.estimatePoseCharucoBoard(ch_corners, ch_ids, self._board, self.camera.K,
-                                                                self.camera.D)
+            if ret > 3:
+                retval, rvec, tvec = aruco.estimatePoseCharucoBoard(ch_corners, ch_ids, self._board, self.camera.K,
+                                                                    self.camera.D)
 
-            # if a pose could be estimated
-            if retval:
-                angle = np.linalg.norm(rvec)
-                axis = rvec.flatten() / angle
+                # if a pose could be estimated
+                if retval:
 
-                quaternion = transformations.quaternion_about_axis(angle, axis)
+                    angle = np.linalg.norm(rvec)
+                    axis = rvec.flatten() / angle
 
-                self._tf_broadcaster.sendTransform(translation=tvec, rotation=quaternion, time=cam_img.header.stamp,
-                                                   child=self._name, parent=cam_img.header.frame_id)
+                    quaternion = transformations.quaternion_about_axis(angle, axis)
+
+                    tag_corners = ch_corners.reshape(4, 2)
+                    c1 = Point()
+                    c1.x = tag_corners[0][0]
+                    c1.y = tag_corners[0][1]
+
+                    c2 = Point()
+                    c2.x = tag_corners[1][0]
+                    c2.y = tag_corners[1][1]
+
+                    c3 = Point()
+                    c3.x = tag_corners[2][0]
+                    c3.y = tag_corners[2][1]
+
+                    c4 = Point()
+                    c4.x = tag_corners[3][0]
+                    c4.y = tag_corners[3][1]
+
+                    tag = MarkerTagDetection()
+                    tag.id = self._board_config["name"]
+                    tag.corners2d = [c1, c2, c3, c4]
+                    tag.tag_size = self._board_config["marker_size"]
+                    tag.pose = PoseStamped()
+                    tag.pose.pose.position = Point(*tvec)
+                    tag.pose.pose.orientation = Quaternion(*quaternion)
+                    tag.pose.header = cam_img.header
+                    self._marker_pub.publish(tag)
+
+                    if self._publish_tf:
+                        self._tf_broadcaster.sendTransform(translation=tvec,
+                                                           rotation=quaternion,
+                                                           time=cam_img.header.stamp,
+                                                           child=self._board_config["name"],
+                                                           parent=cam_img.header.frame_id)
 
 
 # Main function
 if __name__ == '__main__':
     # Initialize the node
-    rospy.init_node('multiple_average_transform_publisher')
+    rospy.init_node('ChArUco board pose node')
 
     # Create the camera publisher
-    tap = ChArUcoBoardNode()
+    ChArUcoBoardNode()
     rospy.spin()
